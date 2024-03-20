7fadba1fdd530db865a177db42cdc18e8eeba6e4
u_shin20 <yooshin0303@dgist.ac.kr>
2024-03-20 17:31:33 +0900

    build

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,5 +1,39 @@
+import '../css/project.css';
+
 function Project() {
-    return <>Project</>;
+    return (
+        <div className="project-container">
+            <div className="h1-container">
+                <h1>Project</h1>
+            </div>
+            <div className="projects-list">
+                <div className="one-project">
+                    <a href="/project">
+                        <p className="title">WOD.I</p>
+                        <p className="description">Crossfit Application</p>
+                    </a>
+                </div>
+                <div className="one-project">
+                    <a href="/project">
+                        <p className="title">Web Portpolio</p>
+                        <p className="description">Online Resume</p>
+                    </a>
+                </div>
+                <div className="one-project">
+                    <a href="/project">
+                        <p className="title">network visualization</p>
+                        <p className="description">ICS network visualization</p>
+                    </a>
+                </div>
+                <div className="one-project">
+                    <a href="/project">
+                        <p className="title">ICS cyber training env.</p>
+                        <p className="description">Establishment of cyber training environment against cyber threats</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Project;
