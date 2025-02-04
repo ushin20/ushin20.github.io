d517baf83715e575d34d33939263d4587dde30e7
u_shin20 <yooshin0303@dgist.ac.kr>
2025-02-04 17:32:50 +0900

    update toolbar

--- js/meta.js
+++ js/meta.js
@@ -28,28 +28,3 @@ link.referrerPolicy = "no-referrer";
 
 // Append the link tag to the document head
 document.head.appendChild(link);
-
-// check topbar, if page-container is collapsed, hidden <p>Ushin20...</p>
-document.addEventListener("DOMContentLoaded", checkTopbar);
-window.addEventListener("resize", checkTopbar);
-
-function isMultiline(container) {
-  const children = Array.from(container.children); // 내부 요소 목록 가져오기
-  if (children.length < 2) return false; // 2개 이상 요소가 있어야 줄바꿈 감지 가능
-
-  // 첫 번째 요소의 offsetTop과 마지막 요소의 offsetTop을 비교
-  return children[0].offsetTop !== children[children.length - 1].offsetTop;
-}
-
-function checkTopbar() {
-  const container = document.querySelector(".pages-container");
-  const topbarText = document.querySelector(".topbar p");
-
-  if (!container || !topbarText) return;
-
-  if (isMultiline(container)) {
-    topbarText.style.display = "none"; // 숨김
-  } else {
-    topbarText.style.display = "block"; // 표시
-  }
-}
