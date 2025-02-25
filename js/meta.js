99448418c7a557fce45ebb4c8b9c810e1bd6894c
u_shin20 <yooshin0303@dgist.ac.kr>
2025-02-25 21:14:12 +0900

    update topbar

--- js/meta.js
+++ js/meta.js
@@ -28,3 +28,37 @@ link.referrerPolicy = "no-referrer";
 
 // Append the link tag to the document head
 document.head.appendChild(link);
+
+// add topbar
+document.addEventListener("DOMContentLoaded", function () {
+  // .frame 요소를 찾기
+  const frameElement = document.querySelector(".frame");
+
+  // 새로운 .topbar 요소 생성
+  const topbarDiv = document.createElement("div");
+  topbarDiv.className = "topbar";
+  topbarDiv.innerHTML = `
+      <a class="logo-container" href="/index.html">
+          <img src="/assets/logo.png" alt="" />
+      </a>
+      <div class="pages-container">
+          <a href="/pages/publication.html">Publications</a>,
+          <a href="/pages/project.html">Projects</a>,
+          <a href="/pages/about.html">About</a>,
+          <a href="/pages/article.html">Article</a>
+      </div>
+      <div class="sites">
+        <a href="/index.html" class="email">
+            <i class="fa-solid fa-house"></i>
+        </a>
+        <a href="https://github.com/ushin20" class="email">
+            <i class="fa-brands fa-git"></i>
+        </a>
+      </div>
+  `;
+
+  // .frame 요소가 존재하면, 맨 앞에 .topbar 추가
+  if (frameElement) {
+    frameElement.prepend(topbarDiv);
+  }
+});
